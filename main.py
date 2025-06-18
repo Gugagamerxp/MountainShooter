65029c82cfc10c230ab1b9b806c7c4cef3f03505
Gugagamerxp <108828995+Gugagamerxp@users.noreply.github.com>
2025-06-18 13:42:18 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+print('setup start')
+
+pygame.init()
+x = 600
+y = 800
+window = pygame.display.set_mode(size=(y, x))
+
+print('setup end')
+
+print('loop start')
+while True:
+    # check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # close window
+            quit()  # end pygame
