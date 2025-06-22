81533b56c3f6a787d43f3fb071d03d8801a95e6a
Gugagamerxp <108828995+Gugagamerxp@users.noreply.github.com>
2025-06-22 10:55:05 -0300

    Creating main loop Connecting Menu and Game classes

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-#  Primeiros conceitos: git,github,intalacao e funcionamento do pygame
+from code.game import Game
 
-import pygame
-
-print('setup start')
-
-pygame.init()
-x = 600
-y = 800
-window = pygame.display.set_mode(size=(y, x))
-
-print('setup end')
-
-print('loop start')
-while True:
-    # check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # close window
-            quit()  # end pygame
-
-
-# Aula pratica 2
-# Diagramacao UMl do jogo
-# Estruturar as classes
-# Criar o menu
-# Criar o background,som e fontes da priemria fase
+game = Game()
+game.run()
 
 
 
