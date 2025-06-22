e8433bf5ab22b9faa3df829e66326cb7803f3cc3
Gugagamerxp <108828995+Gugagamerxp@users.noreply.github.com>
2025-06-22 13:29:36 -0300

    Creating asset folder Inserting all assets Creating the menu: background , sound and fonts Creating the const file

---
+++ code/const.py
@@ -0,0 +1,15 @@
+# C
+COLOR_PURPLE = (162, 0, 255)
+COLOR_LIGHT_GREEN = (74, 185, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,71 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import WIN_WIDTH, COLOR_LIGHT_GREEN, COLOR_PURPLE, MENU_OPTION, COLOR_WHITE
+
+
 class Menu:
-    def __init__(self,window):
+    def __init__(self, window):
         self.window = window
 
+        # Trazendo imagem para projeto e slavando no objeto surf:
+
+        self.surf = pygame.image.load('./asset/menuBg.png')
+
+        # Criando um objeto( rect) e utilizando o objeto com a imagem
+        # para fazer um retangulo onde a imagem ficara passando parametros(left,top)
+
+        self.rect = self.surf.get_rect(left=0, top=0)
+
     def run(self, ):
-        pass
+
+        # Adicionando musica de fundo com o pygame.mixer_music.load
+        # Lembrando que aqui a musica somente foi carregada
+        pygame.mixer_music.load('./asset/menu_music.mp3')
+
+        # Reproduzindo a musica indefinidamente (parametro -')
+        pygame.mixer_music.play(-1)
+
+        while True:
+
+            # Depois de criar a imagem e preciso desenha-la para poder
+            # usar a janela. E para isso se usa o metodo blit()
+
+            self.window.blit(source=self.surf, dest=self.rect)
+
+            # Escrevendo texto na janela
+            # Lembrar sempre de manter a ordem primeiro precisa desenhar o fundo (linha 41),
+            # para depois mostrar o texto na tela(linha 46)
+
+            self.menu_text(60, "City", COLOR_PURPLE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(60, "Shooter", COLOR_LIGHT_GREEN, ((WIN_WIDTH / 2), 130))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 26 * i))
+
+            # Mesmo apos isso tudo e preciso usar um metodo para poder atualizar a tela
+            # utilizando o metodo pygame.diaplay.flip
+
+            pygame.display.flip()
+
+            # ob : se a imagem nao estiver do tamanho certo da janela(muito provavel),
+            # lembrar de mudar o tamanho da janela ou redimensionar a imagem para o
+            # tamanho da janela.
+
+            # check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # close window
+                    quit()  # end pygame
+
+    # Metodo para criar  texto do menu
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
